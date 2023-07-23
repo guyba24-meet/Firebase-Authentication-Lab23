39716f1eae853907a51577ec30c03e07ad06ac50
guyba24-meet <139680985+guyba24-meet@users.noreply.github.com>
2023-07-23 18:27:49 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,18 +2,51 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+
+config = {"apiKey": "AIzaSyCUzAkwdm04S38pPaWSgN6NW-yk5MfZgVg",
+  "authDomain": "guy-s-meet-project1.firebaseapp.com",
+  "projectId": "guy-s-meet-project1",
+  "storageBucket": "guy-s-meet-project1.appspot.com",
+  "messagingSenderId": "591357375724",
+  "appId": "1:591357375724:web:3ac93c33bfb6bb5faca1c9",
+  "databaseURL": ""}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email,password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Authentication failed"
+            return render_template("signin.html")
+    else:
+        return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Authentication failed"
+            return render_template("signup.html")
+    else:
+        return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
